10d6b239082103f8ebd5e5082c61b8f6f05f0f0d
IagoMendes <iagorainhamendes@hotmail.com>
2018-04-10 09:12:42 -0300

    organizando smach

--- Scripts/logica_estado_smach.py
+++ Scripts/logica_estado_smach.py
@@ -1,3 +1,83 @@
+import smach
+import smach_ros
+
+## Importar a classe de cada arquivo ou criar as classes direto nesse codigo
+
+## Classes para o state machine
+#Classe para procurar o objeto
+class Procurar():
+  def __init__(self):
+    smach.State.__init__(self, outcomes=['objeto_1', 'objeto_2', 'nada'])
+
+  def execute(self, userdata):
+  
+    return 'objeto_1'
+    
+    return 'objeto_2'
+    
+    return 'nada'     
+
+#Apos encontrar o objeto, essa classe serve para seguir o objeto
+class Seguir():
+  def __init__(self):
+    smach.State.__init__(self, outcomes=['longe', 'desviar', 'perto'])
+
+  def execute(self, userdata):
+  
+    return 'longe'
+    
+    return 'desviar'
+    
+    return 'perto'     
+
+#Classe utilizada para desviar de objetos perto do robo
+class Desviar():
+  def __init__(self):
+    smach.State.__init__(self, outcomes=['desviado', 'desviando'])
+
+  def execute(self, userdata):
+  
+    return 'desviado'
+    
+    return 'desviando'
+
+#As duas classes abaixo servem para tocar sons do robo
+class Som1():
+def __init__(self):
+    smach.State.__init__(self, outcomes=['tocado'])
+
+  def execute(self, userdata):
+  
+    return 'tocado'
+
+
+class Som2():
+def __init__(self):
+    smach.State.__init__(self, outcomes=['tocado'])
+
+  def execute(self, userdata):
+  
+    return 'tocado'    
+    
+#Essa classe vira 180 graus
+class Virar():
+def __init__(self):
+    smach.State.__init__(self, outcomes=['virado'])
+
+  def execute(self, userdata):
+  
+    return 'virado'
+
+#Essa classe gira 360 graus
+class Girar():
+def __init__(self):
+    smach.State.__init__(self, outcomes=['girado'])
+
+  def execute(self, userdata):
+  
+    return 'girado'
+
+
 def main():
     global velocidade_saida
     rospy.init_node('smach_example_state_machine')
