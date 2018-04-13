c2e1d26b6a9a4ce4519e5e1b2e264bac13b3c2c3
LiuSeeker <vitor.liu@gmail.com>
2018-04-13 10:06:53 -0300

    mod smach

--- Scripts/Face2.py
+++ Scripts/Face2.py
@@ -14,75 +14,146 @@ from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+import smach
+import smach_ros
+
 import cormodule
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
 bridge = CvBridge()
 
+global cv_image
+global dif_x
+global media
+global centro
 cv_image = None
+dif_x = None
+
+# Variáveis para permitir que o roda_todo_frame troque dados com a máquina de estados
+media = 0
+centro = 0
+
 
 
 def roda_todo_frame(imagem):
+	print("frame")
 	global cv_image
-	global velocidade_saida
+	global media
+	global centro
+	global dif_x
 
-	cap = cv2.VideoCapture(0)
-
-	ret, img = cap.read()
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+	rospy.sleep(0.5)
+	gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+	print(gray)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+	cv2.imshow("Camera", cv_image)
+	
 	for(x,y,z,w) in faces:
-		cv2.rectangle(img, (x,y), (x+z, y+w), (255,0,0), 2)
+		cv2.rectangle(cv_image, (x,y), (x+z, y+w), (255,0,0), 2)
 		roi_gray = gray[y:y+w, x:x+z]
-		roi_color = img[y:y+w, x:x+z]
+		roi_color = cv_image[y:y+w, x:x+z]
 
-		tolerancia = 50
 		media = x+z/2
-		centro = img.shape[1]//2
+		centro = cv_image.shape[0]//2
 
 		if media != 0 and centro != 0:
 			dif_x = media-centro
-		if dif_x > tolerancia:
-			# Vira para a direita
-			velocidade = Twist(Vector3(0.1,0,0), Vector3(0,0,-0.2))
-			print("Vira direita")
-		elif dif_x < -tolerancia:
-			# Vira para a esquerda
-			velocidade = Twist(Vector3(0.1,0,0), Vector3(0,0,0.2))
-			print("Vira esquerda")
-		elif dif_x <= tolerancia and dif_x >= -tolerancia:
-			# Anda, se centralizado
-			print("centralizado")
-			velocidade = Twist(Vector3(0.5,0,0), Vector3(0,0,0))
-			dist = 20*3.04/w
-			print(dist)
 		else:
-			print("Nada")
-			velocidade = Twist(Vector3(0,0,0), Vector3(0,0,0))
-
-	velocidade_saida.publish(velocidade)
+			dif_x = None
 
 
-	
 
 
-if __name__=="__main__":
+## Classes - estados
+
+
+class Girando(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['alinhou', 'girando'])
+
+    def execute(self, userdata):
+		global velocidade_saida
+
+		tolerancia = 50
+
+		if  dif_x > tolerancia:
+			velocidade = Twist(Vector3(0.1,0,0), Vector3(0,0,-0.2))
+			velocidade_saida.publish(velocidade)
+			return 'girando'
+		elif dif_x < -tolerancia:
+			velocidade = Twist(Vector3(0.1,0,0), Vector3(0,0,0.2))
+			velocidade_saida.publish(velocidade)
+			return 'girando'
+		elif dif_x > -tolerancia and dif_x < tolerancia:
+			velocidade = Twist(Vector3(0.5,0,0), Vector3(0,0,0))
+			velocidade_saida.publish(velocidade)
+			return 'alinhou'
+		else:
+			velocidade = Twist(Vector3(0,0,0), Vector3(0,0,0.5))
+			velocidade_saida.publish(velocidade)
+			return 'girando'
+
+
+class Centralizado(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['alinhando', 'alinhado'])
+
+    def execute(self, userdata):
+		global velocidade_saida
+
+		tolerancia = 50
+
+		if  dif_x > tolerancia:
+			return 'alinhando'
+		elif dif_x < -tolerancia:
+			return 'alinhando'
+		elif dif_x > -tolerancia and dif_x < tolerancia:
+			print(dif_x)
+			velocidade = Twist(Vector3(0.5,0,0), Vector3(0,0,0))
+			velocidade_saida.publish(velocidade)
+			return 'alinhado'
+		else:
+			return 'alinhando'
+
+# main
+def main():
 	global velocidade_saida
+	global buffer
+	rospy.init_node('cor_estados')
 
-	rospy.init_node("cor")
-
-	# Para usar a Raspberry Pi
-	topico_raspberry_camera = "/raspicam_node/image/compressed"
-
-	topico_imagem = topico_raspberry_camera
-
-	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+	# Para usar a webcam 
+	recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
+	#recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
+	# Create a SMACH state machine
+	sm = smach.StateMachine(outcomes=['terminei'])
 
-	while not rospy.is_shutdown():
-		rospy.sleep(0.5)
+	# Open the container
+	with sm:
+	    # Add states to the container
+	    #smach.StateMachine.add('LONGE', Longe(), 
+	    #                       transitions={'ainda_longe':'ANDANDO', 
+	    #                                    'perto':'terminei'})
+	    #smach.StateMachine.add('ANDANDO', Andando(), 
+	    #                       transitions={'ainda_longe':'LONGE'})
+	    smach.StateMachine.add('GIRANDO', Girando(),
+	                            transitions={'girando': 'GIRANDO',
+	                            'alinhou':'CENTRO'})
+	    smach.StateMachine.add('CENTRO', Centralizado(),
+	                            transitions={'alinhando': 'GIRANDO',
+	                            'alinhado':'CENTRO'})
 
+
+	# Execute SMACH plan
+	outcome = sm.execute()
+	#rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
--- Scripts/logica_estado_smach.py
+++ Scripts/logica_estado_smach.py
@@ -3,88 +3,175 @@ import smach_ros
 
 ## Importa cada uma das funcoes de seus respectivos arquivos
 
-import desviar
+
+def converte(valor):
+		return valor*44.4/0.501
+
+def roda_todo_frame(imagem):
+	global cv_image
+	global velocidade_saida
+	global media
+	global centro
+
+	cap = cv2.VideoCapture(0)
+
+	ret, imagem = cap.read()
+	cv_image = bridge.compressed_imagemmsg_to_cv2(imagem, "bgr8")
+	gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	cv2.imshow("Camera", cv_image)
+
+	for(x,y,z,w) in faces:
+		cv2.rectangle(imagem, (x,y), (x+z, y+w), (255,0,0), 2)
+		roi_gray = gray[y:y+w, x:x+z]
+		roi_color = imagem[y:y+w, x:x+z]
+
+		media = x+z/2
+		centro = imagem.shape[1]//2
+
+
 
 ## Classes para o state machine
 #Classe para procurar o objeto
 class Procurar(smash.State):
 
-  def __init__(self):
-    smach.State.__init__(self, outcomes=['objeto_1', 'objeto_2', 'nada'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['objeto_1', 'objeto_2', 'nada'])
 
-  def execute(self, userdata):
-  
-    return 'objeto_1'
+	def execute(self, userdata):
+		  
+    	return 'objeto_1'
     
-    return 'objeto_2'
+    	return 'objeto_2'
     
-    return 'nada'     
+    	return 'nada'     
 
 #Apos encontrar o objeto, essa classe serve para seguir o objeto
 class Seguir(smash.State):
-  def __init__(self):
-    smach.State.__init__(self, outcomes=['longe', 'desviar', 'perto'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['longe', 'desviar', 'perto'])
 
-  def execute(self, userdata):
+	def execute(self, userdata):
   
-    return 'longe'
+    	return 'longe'
     
-    return 'desviar'
+    	return 'desviar'
     
-    return 'perto'     
+    	return 'perto'     
 
 #Classe utilizada para desviar de objetos perto do robo
 class Desviar(smash.State):
-  def __init__(self):
-    smach.State.__init__(self, outcomes=['desviado', 'desviando'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['desviado', 'desviando'])
 
+  	def execute(self, userdata):
+  		distancias = np.array(dado.ranges)
+		desviando = False
 
-  def execute(self, userdata):
-    return desviar.scaneou()
+		for i in range(len(distancias)):
+
+			if i <= 40:
+				if converte(distancias[i]) < 50 and converte(distancias[i]) >= 30:
+					velocidade = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, -0.7))
+					desviando = True
+				elif converte(distancias[i]) < 30 and  converte(distancias[i]) != 0:
+					velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, -0.9))
+					desviando = True
+				
+			if i >= 320:
+				if converte(distancias[i]) < 50 and converte(distancias[i]) >= 30:
+					velocidade = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, 0.7))
+					desviando = True
+				elif converte(distancias[i]) < 30 and  converte(distancias[i]) != 0:
+					velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, 0.9))
+					desviando = True
+				
+			if i <= 70 and i > 40:
+				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
+					velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, -0.5))
+					desviando = True
+
+			if i < 320 and i >= 290:
+				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
+					velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, 0.5))
+					desviando = True
+
+			if i < 290 and i > 70:
+				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
+					velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.5))
+					desviando = True
+
+		if velocidade != None:
+			velocidade_saida.publish(velocidade)
+			return 'desviando'
+		else:
+			return 'desviado'
 
 #As duas classes abaixo servem para tocar sons do robo
 class Som1(smash.State):
-def __init__(self):
-    smach.State.__init__(self, outcomes=['tocado'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['tocado'])
 
-  def execute(self, userdata):
-  
-    return 'tocado'
+  	def execute(self, userdata):
+  		saida_som.publish(0)
+    	return 'tocado'
 
 
 class Som2(smash.State):
-def __init__(self):
-    smach.State.__init__(self, outcomes=['tocado'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['tocado'])
 
-  def execute(self, userdata):
-  
-    return 'tocado'    
+  	def execute(self, userdata):
+  		saida_som.publish(2)
+    	return 'tocado'    
     
 #Essa classe vira 180 graus
 class Virar(smash.State):
-def __init__(self):
-    smach.State.__init__(self, outcomes=['virado'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['virado'])
 
-  def execute(self, userdata):
-  
-    return 'virado'
+  	def execute(self, userdata):
+  		quat = dado.orientation
+		lista = [quat.x, quat.y, quat.z, quat.w]
+		angulos = np.degrees(transformations.euler_from_quaternion(lista))
+
+		ang_inicial, ang_final = angulos_inicial_e_final()
+
+		ang_atual = angulos[0]
+
+		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.7))
+
+		if ang_atual <= ang_final+3 and ang_atual >= ang_final-3:
+			velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+
+		velocidade_saida.publish(velocidade)
+    	return 'virado'
 
 #Essa classe gira 360 graus
 class Girar(smash.State):
-def __init__(self):
-    smach.State.__init__(self, outcomes=['girado'])
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['girado'])
 
-  def execute(self, userdata):
+  	def execute(self, userdata):
   
-    return 'girado'
+    	return 'girado'
 
 
 def main():
     global velocidade_saida
+    global ang_inicial
+	global ang_final
     rospy.init_node('smach_example_state_machine')
     #recebedor = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, recebe)
-    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+	saida_som = rospy.Publisher("/sound", Sound, queue_size = 2)
+
+	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
+
+    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 2)
     # trazer o recebedor de laser para cá
+    recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+
+
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['terminei'])
@@ -122,6 +209,7 @@ def main():
     # Execute SMACH plan
     outcome = sm.execute()
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 if _name_ == '_main_':
     main()
