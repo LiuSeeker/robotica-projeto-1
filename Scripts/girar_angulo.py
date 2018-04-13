f0e048bdbde932744a6a13da8b801d442e80ffb6
Vitor Satyro <vitorsv1@al.insper.edu.br>
2018-04-13 07:28:48 -0300

    Mudei umas  coisas no codigo

--- Scripts/girar_angulo.py
+++ Scripts/girar_angulo.py
@@ -4,28 +4,23 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+def frange(start, stop, step=1.0):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
 
 def girar():
-	error = 0.05
 	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 	twist = Twist()
 
 	while not rospy.is_shutdown():
-		msg = rospy.wait_for_message("/scan", LaserScan)
-		scan_filter = []
-		for i in range(360):
-			if i <= 15 or i > 335:
-				if msg.range[i] >= error:
-					self.scan_filter.append(msg.range[i])
-
-		if min(scan_filter) < 0.3:
-			rospy.loginfo('Ta mto perto')
-			twist.linear.x = 0.0
-			twist.angular.z = 0
-			for i in range(0.8,0,-0.001):
-				twist.angular.z += i 
-				rospy.loginfo('Girando')
-			rospy.loginfo('Girou')
+		twist.angular.z = 0
+		for i in frange(0.8,0,-0.1):
+			twist.angular.z = i 
+			rospy.loginfo('Girando')
+		rospy.loginfo('Girou')
 
 		pub.publish(twist)
 
