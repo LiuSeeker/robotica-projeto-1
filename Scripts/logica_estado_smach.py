739070a12f12b2c8f8a1019ae93ff31140b1fffc
LiuSeeker <vitor.liu@gmail.com>
2018-04-16 12:44:29 -0300

    b

--- Scripts/logica_estado_smach.py
+++ Scripts/logica_estado_smach.py
@@ -29,7 +29,17 @@ def roda_todo_frame(imagem):
 		media = x+z/2
 		centro = imagem.shape[1]//2
 
+def scaneou(dado):
+	global distancias
+	#print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+	#print("Leituras:")
+	distancias = np.array(dado.ranges)
 
+def leu_imu(dado):
+	global angulos
+	quat = dado.orientation
+	lista = [quat.x, quat.y, quat.z, quat.w]
+	angulos = np.degrees(transformations.euler_from_quaternion(lista))
 
 ## Classes para o state machine
 #Classe para procurar o objeto
@@ -39,12 +49,16 @@ class Procurar(smash.State):
     	smach.State.__init__(self, outcomes=['objeto_1', 'objeto_2', 'nada'])
 
 	def execute(self, userdata):
-		  
-    	return 'objeto_1'
-    
-    	return 'objeto_2'
-    
-    	return 'nada'     
+		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.3))
+		
+		##if acha objeto 1:
+    		return 'objeto_1'
+    	
+    	##if acha objeto 1:
+    		return 'objeto_2'
+    	
+    	else: ##if nao acha nd:
+    		return 'nada'     
 
 #Apos encontrar o objeto, essa classe serve para seguir o objeto
 class Seguir(smash.State):
@@ -65,8 +79,6 @@ class Desviar(smash.State):
     	smach.State.__init__(self, outcomes=['desviado', 'desviando'])
 
   	def execute(self, userdata):
-  		distancias = np.array(dado.ranges)
-		desviando = False
 
 		for i in range(len(distancias)):
 
@@ -116,46 +128,48 @@ class Som1(smash.State):
   		saida_som.publish(0)
     	return 'tocado'
 
-
 class Som2(smash.State):
 	def __init__(self):
     	smach.State.__init__(self, outcomes=['tocado'])
 
   	def execute(self, userdata):
   		saida_som.publish(2)
-    	return 'tocado'    
-    
+    	return 'tocado'
+
+#Classe que, quando rodada, grava o angulo atual como o inicial e defini o angulo final
+class Pos_ini(smash.State):
+	def __init__(self):
+    	smach.State.__init__(self, outcomes=['pego'])
+
+    def execute(self, userdata):
+    	global ang_inicial
+		global ang_final
+		ang_inicial = angulos[0]
+
+		if ang_inicial < 180 and ang_inicial >= 0:
+			ang_final = ang_inicial - 180
+		elif ang_inicial > -180 and ang_inicial < 0:
+			ang_final = ang_inicial + 180
+
+		return 'pego'
+
 #Essa classe vira 180 graus
 class Virar(smash.State):
 	def __init__(self):
-    	smach.State.__init__(self, outcomes=['virado'])
+    	smach.State.__init__(self, outcomes=['virando', 'virado'])
 
   	def execute(self, userdata):
-  		quat = dado.orientation
-		lista = [quat.x, quat.y, quat.z, quat.w]
-		angulos = np.degrees(transformations.euler_from_quaternion(lista))
-
-		ang_inicial, ang_final = angulos_inicial_e_final()
-
-		ang_atual = angulos[0]
+  		ang_atual = angulos[0]
 
 		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.7))
 
-		if ang_atual <= ang_final+3 and ang_atual >= ang_final-3:
-			velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
-
 		velocidade_saida.publish(velocidade)
-    	return 'virado'
 
-#Essa classe gira 360 graus
-class Girar(smash.State):
-	def __init__(self):
-    	smach.State.__init__(self, outcomes=['girado'])
+		if ang_atual <= ang_final+3 and ang_atual >= ang_final-3:
+			return 'virado'
 
-  	def execute(self, userdata):
-  
-    	return 'girado'
-
+		else:
+			return 'virando'	
 
 def main():
     global velocidade_saida
@@ -171,7 +185,7 @@ def main():
     # trazer o recebedor de laser para cá
     recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
 
-
+    recebe_imu = rospy.Subscriber("/imu", Imu, leu_imu)
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['terminei'])
@@ -182,7 +196,7 @@ def main():
         smach.StateMachine.add('PROCURAR', Procurar(), 
                                transitions={'objeto_1':'SEGUIR', #se achar o objeto 1, retorna 'objeto_1' e roda 'SEGUIR'
                                             'objeto_2':'SOM_2', #se achar o objeto 2, retorna 'objeto_2' e roda 'SOM_2'
-                                            'nada': 'GIRAR'}) #se não achar nada, retorna 'nada' e roda "GIRAR"
+                                            'nada': 'PROCURAR'}) #se não achar nada, retorna 'nada' e roda "VIRAR"
 
         smach.StateMachine.add('SEGUIR', Seguir(), 
                                transitions={'longe':'SEGUIR', #se nao scnear nada perto, estiver longe do objeto e estiver com o objeto centralizado, retorna 'longe' e roda 'SEGUIR'
@@ -197,14 +211,14 @@ def main():
                                transitions={'tocado': 'PROCURAR'}) #toca o som1, e retorna 'tocado' e roda 'PROCURAR'
       
         smach.StateMachine.add('SOM_2', Som2(), 
-                               transitionsd={'tocado': 'VIRAR'}) #toca o som2, e retorna 'tocado' e roda 'VIRAR'
-      
-        smach.StateMachine.add('VIRAR', Virar(), 
-                               transitions={'virado': 'PROCURAR'}) #vira 180 graus, e retorna 'virado' e roda 'PROCURAR'
-      
-        smach.StateMachine.add('GIRAR', Girar(), 
-                               transitions={'girado': 'PROCURAR'}) #gira 360 graus, e retorna 'girado' e roda 'PROCURAR'
+                               transitionsd={'tocado': 'POS_INI'}) #toca o som2, e retorna 'tocado' e roda 'VIRAR'
 
+      	smach.StateMachine.add('POS_INI', Pos_ini(), 
+                               transitionsd={'pego': 'VIRAR'})
+
+        smach.StateMachine.add('VIRAR', Virar(), 
+                               transitions={'virado': 'PROCURAR',
+                               				'virando': 'VIRAR'}) #vira 180 graus, e retorna 'virado' e roda 'PROCURAR'
 
     # Execute SMACH plan
     outcome = sm.execute()
