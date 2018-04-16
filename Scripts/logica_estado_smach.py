05028f60a003d7326b2d15cdaff2dc48d59fb82e
IagoMendes <iagorainhamendes@hotmail.com>
2018-04-16 11:05:26 -0300

    Corrigindo erros no smach

--- Scripts/logica_estado_smach.py
+++ Scripts/logica_estado_smach.py
@@ -3,7 +3,6 @@ import smach_ros
 
 ## Importa cada uma das funcoes de seus respectivos arquivos
 
-
 def converte(valor):
 		return valor*44.4/0.501
 
@@ -29,9 +28,11 @@ def roda_todo_frame(imagem):
 		media = x+z/2
 		centro = imagem.shape[1]//2
 
-
+# Treinamento de reconhecimento facial  
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 ## Classes para o state machine
+
 #Classe para procurar o objeto
 class Procurar(smash.State):
 
@@ -65,47 +66,47 @@ class Desviar(smash.State):
     	smach.State.__init__(self, outcomes=['desviado', 'desviando'])
 
   	def execute(self, userdata):
-  		distancias = np.array(dado.ranges)
-		desviando = False
+      distancias = np.array(dado.ranges)
+      desviando = False
 
-		for i in range(len(distancias)):
+      for i in range(len(distancias)):
 
-			if i <= 40:
-				if converte(distancias[i]) < 50 and converte(distancias[i]) >= 30:
-					velocidade = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, -0.7))
-					desviando = True
-				elif converte(distancias[i]) < 30 and  converte(distancias[i]) != 0:
-					velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, -0.9))
-					desviando = True
-				
-			if i >= 320:
-				if converte(distancias[i]) < 50 and converte(distancias[i]) >= 30:
-					velocidade = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, 0.7))
-					desviando = True
-				elif converte(distancias[i]) < 30 and  converte(distancias[i]) != 0:
-					velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, 0.9))
-					desviando = True
-				
-			if i <= 70 and i > 40:
-				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
-					velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, -0.5))
-					desviando = True
+  			if i <= 40:
+  				if converte(distancias[i]) < 50 and converte(distancias[i]) >= 30:
+  					velocidade = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, -0.7))
+  					desviando = True
+  				elif converte(distancias[i]) < 30 and  converte(distancias[i]) != 0:
+  					velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, -0.9))
+  					desviando = True
+  				
+  			if i >= 320:
+  				if converte(distancias[i]) < 50 and converte(distancias[i]) >= 30:
+  					velocidade = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, 0.7))
+  					desviando = True
+  				elif converte(distancias[i]) < 30 and  converte(distancias[i]) != 0:
+  					velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, 0.9))
+  					desviando = True
+  				
+  			if i <= 70 and i > 40:
+  				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
+  					velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, -0.5))
+  					desviando = True
 
-			if i < 320 and i >= 290:
-				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
-					velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, 0.5))
-					desviando = True
+  			if i < 320 and i >= 290:
+  				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
+  					velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, 0.5))
+  					desviando = True
 
-			if i < 290 and i > 70:
-				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
-					velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.5))
-					desviando = True
+  			if i < 290 and i > 70:
+  				if converte(distancias[i]) < 25 and converte(distancias[i]) != 0:
+  					velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.5))
+  					desviando = True
 
-		if velocidade != None:
-			velocidade_saida.publish(velocidade)
-			return 'desviando'
-		else:
-			return 'desviado'
+  		if velocidade != None:
+  			velocidade_saida.publish(velocidade)
+  			return 'desviando'
+  		else:
+  			return 'desviado'
 
 #As duas classes abaixo servem para tocar sons do robo
 class Som1(smash.State):
@@ -131,21 +132,21 @@ class Virar(smash.State):
     	smach.State.__init__(self, outcomes=['virado'])
 
   	def execute(self, userdata):
-  		quat = dado.orientation
-		lista = [quat.x, quat.y, quat.z, quat.w]
-		angulos = np.degrees(transformations.euler_from_quaternion(lista))
+  	quat = dado.orientation
+  		lista = [quat.x, quat.y, quat.z, quat.w]
+  		angulos = np.degrees(transformations.euler_from_quaternion(lista))
 
-		ang_inicial, ang_final = angulos_inicial_e_final()
+  		ang_inicial, ang_final = angulos_inicial_e_final()
 
-		ang_atual = angulos[0]
+  		ang_atual = angulos[0]
 
-		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.7))
+  		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.7))
 
-		if ang_atual <= ang_final+3 and ang_atual >= ang_final-3:
-			velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+  		if ang_atual <= ang_final+3 and ang_atual >= ang_final-3:
+  			velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
 
-		velocidade_saida.publish(velocidade)
-    	return 'virado'
+  		velocidade_saida.publish(velocidade)
+      	return 'virado'
 
 #Essa classe gira 360 graus
 class Girar(smash.State):
@@ -160,17 +161,16 @@ class Girar(smash.State):
 def main():
     global velocidade_saida
     global ang_inicial
-	global ang_final
+    global ang_final
     rospy.init_node('smach_example_state_machine')
     #recebedor = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, recebe)
-	saida_som = rospy.Publisher("/sound", Sound, queue_size = 2)
+    saida_som = rospy.Publisher("/sound", Sound, queue_size = 2)
 
-	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
+    recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
 
     velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 2)
     # trazer o recebedor de laser para cá
     recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
-
 
 
     # Create a SMACH state machine
@@ -205,11 +205,9 @@ def main():
         smach.StateMachine.add('GIRAR', Girar(), 
                                transitions={'girado': 'PROCURAR'}) #gira 360 graus, e retorna 'girado' e roda 'PROCURAR'
 
-
     # Execute SMACH plan
     outcome = sm.execute()
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 if _name_ == '_main_':
     main()
