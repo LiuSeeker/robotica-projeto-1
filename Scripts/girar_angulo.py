664b4006308e1adbd7d83f4d3f4c949701b8b4b3
Vitor Satyro <vitorsv1@al.insper.edu.br>
2018-04-15 21:10:20 -0300

    Mudei a funcao, agora funciona

--- Scripts/girar_angulo.py
+++ Scripts/girar_angulo.py
@@ -4,34 +4,23 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-def frange(start, stop, step=1.0):
-    i = []
-    while start > stop:
-    	start += step
-    	i.append(start)
-    return i
-
-def girar():
-	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-	twist = Twist()
-	twist.angular.z = 0
-	twist.linear.z = 0
-	#Esse for determina o quanto ele vai virar
-	for i in frange(0.8,0,-1e-5):
-		twist.linear.z = i
-		twist.angular.z = i 
-		rospy.loginfo('Girando')
-	rospy.loginfo('Girou')
-
-	pub.publish(twist)
+def girar(dado):
+	global velocidade_saida
+	global gira
+	rospy.loginfo('GIRANDO')
+	#comando para girar
+	if abs(gira) < 3:
+		vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.3))
+		velocidade_saida.publish(vel)
+	else:
+		vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, girar * 0.5))
+		velocidade_saida.publish(vel)
+	if girar != 0:
+		if girar < 0: 
+			girar += 1
+		else: 
+			girar -= 1
+		return "Girando"
 
 
-def main():
-    rospy.init_node('girar_angulo')
-    try:
-        obstacle = girar()
-    except rospy.ROSInterruptException:
-        pass
 
-if __name__ == '__main__':
-    main()
